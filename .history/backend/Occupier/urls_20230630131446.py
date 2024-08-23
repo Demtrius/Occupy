06c4f3d0b1f73e0d75841862378c4788c5b290df
Samuel ihuoma <samuelihuoma@Samuels-MacBook-Pro.local>
2024-08-23 16:54:34 +0100

    post create

--- .history/backend/Occupier/urls_20230630131446.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView,OccupierLoginView
-from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token, TokenObtainPairView,TokenRefreshView,TokenVerifyView
-app_name = 'Occupier'
-
-urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login/',OccupierLoginView.as_view()),
-]
---
+++ .history/backend/Occupier/urls_20230807134756.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import RegisterView,OccupierLoginView,OccupierListView,OccupierViewSet
+from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView, TokenVerifyView)
+app_name = 'Occupier'
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login/',OccupierLoginView.as_view()),
+    # path("jwt/create/",TokenObtainPairView.as_view(),name='jwt_create')
+    path("jwt/create/", TokenObtainPairView.as_view(), name="jwt_create"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path('occupier-list/',OccupierListView.as_view(), name="occupier")
+]
