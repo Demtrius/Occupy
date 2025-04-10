0cfa02dc7fe40c014f9eef8f79c2c569ffa72052
Demtrius <samihuomaoccupy@gmail.com>
2025-04-10 17:34:51 +0100

    Fix database configuration for Heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,8 +158,11 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '5432 ',
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
+    
     }
 
 
