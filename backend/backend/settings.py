74606e045fc79db4a04c19e2839b5b2e4c37b4bf
Demtrius <samihuomaoccupy@gmail.com>
2025-05-04 16:24:01 +0200

    'dockerizing'

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,10 @@ SECRET_KEY = 'django-insecure-g^*rc8ie0tilq&foi0p4ft+&y+8c3heosv+-j-fh%l6wpz9-i^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
- ALLOWED_HOSTS = [*]
+ ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+ ]
 
 
 
